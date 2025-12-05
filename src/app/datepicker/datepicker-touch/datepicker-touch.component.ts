0e5c14c5d63df18022006c25fb88887c2b93de33
SYLVAIN.FRAGNIER <sylvain.fragnier@asp-public.fr>
2025-12-05 09:11:13 +0100

    ajout pb datepicker

--- src/app/datepicker/datepicker-touch/datepicker-touch.component.ts
+++ src/app/datepicker/datepicker-touch/datepicker-touch.component.ts
@@ -1,15 +1,15 @@
 import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
-import {FormControl, ReactiveFormsModule, TouchedChangeEvent} from '@angular/forms';
+import {FormControl, ReactiveFormsModule} from '@angular/forms';
 import {DsfrDatePickerComponent} from "@edugouvfr/ngx-dsfr-ext";
-import {filter} from "rxjs";
-import {DsfrButtonComponent} from "@edugouvfr/ngx-dsfr";
+import {DsfrAccordionComponent, DsfrButtonComponent} from "@edugouvfr/ngx-dsfr";
 
 @Component({
     selector: 'app-datepicker-touch',
     imports: [
         DsfrDatePickerComponent,
         ReactiveFormsModule,
-        DsfrButtonComponent
+        DsfrButtonComponent,
+        DsfrAccordionComponent
     ],
     templateUrl: './datepicker-touch.component.html',
     styleUrl: './datepicker-touch.component.css',
@@ -17,22 +17,10 @@ import {DsfrButtonComponent} from "@edugouvfr/ngx-dsfr";
 })
 export class DatepickerTouchComponent implements AfterViewInit {
     control1 = new FormControl();
-    control1Evts: string[] = []
     control2 = new FormControl();
-    control2Evts: string[] = []
 
     @ViewChild('datepicker2', {read: ElementRef})
     datepicker2!: ElementRef<HTMLElement>;
-
-    constructor() {
-        this.control1.events.pipe(filter(evt => evt instanceof TouchedChangeEvent)).subscribe((evt) => {
-            this.control1Evts.push(`TouchedChangeEvent : touched=${evt.touched}`);
-        })
-
-        this.control2.events.pipe(filter(evt => evt instanceof TouchedChangeEvent)).subscribe((evt) => {
-            this.control2Evts.push(`TouchedChangeEvent : touched=${evt.touched}`);
-        })
-    }
 
     ngAfterViewInit(): void {
         this.datepicker2.nativeElement.querySelector('input[type="text"]')?.addEventListener('blur', () => {
